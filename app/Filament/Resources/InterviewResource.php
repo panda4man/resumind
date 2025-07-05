9ce9e81866014f17acbd2450e01e67875d9e14ec
Andrew Clinton <ddrew.clinton@gmail.com>
2025-07-04 23:00:02 -0400

    DRY form fields

--- app/Filament/Resources/InterviewResource.php
+++ app/Filament/Resources/InterviewResource.php
@@ -2,8 +2,11 @@
 
 namespace App\Filament\Resources;
 
+use App\Enums\InterviewFormatsEnum;
+use App\Enums\InterviewTypesEnum;
 use App\Filament\Resources\InterviewResource\Pages;
 use App\Models\Interview;
+use Filament\Forms\Components;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
@@ -19,9 +22,7 @@ class InterviewResource extends Resource
     public static function form(Form $form): Form
     {
         return $form
-            ->schema([
-                //
-            ]);
+            ->schema(static::getSharedFormFields());
     }
 
     public static function table(Table $table): Table
@@ -65,6 +66,38 @@ class InterviewResource extends Resource
             TextColumn::make('type')->badge(),
             TextColumn::make('format')->badge(),
             TextColumn::make('length_human_readable')->label('Duration'),
+        ];
+    }
+
+    public static function getSharedFormFields(): array
+    {
+        return [
+            Components\TextInput::make('name')
+                ->required()
+                ->maxLength(255)
+                ->label('Name'),
+            Components\DateTimePicker::make('interview_date')
+                ->required()
+                ->label('Date'),
+            Components\Select::make('type')
+                ->options(collect(InterviewTypesEnum::cases())->mapWithKeys(function ($enum) {
+                    return [$enum->value => $enum->name];
+                }))
+                ->required(),
+            Components\Select::make('format')
+                ->options(collect(InterviewFormatsEnum::cases())->mapWithKeys(function ($enum) {
+                    return [$enum->value => $enum->name];
+                }))
+                ->required(),
+            Components\Select::make('length_minutes')
+                ->label('Duration (minutes)')
+                ->options(
+                    collect(range(5, 180, 5))->mapWithKeys(fn($v) => [
+                        $v => str_pad(floor($v / 60) . 'h ', 4, ' ', STR_PAD_RIGHT) . str_pad($v % 60 . 'm', 3, '0', STR_PAD_LEFT)
+                    ])
+                )
+                ->required(),
+            Components\Textarea::make('notes')
         ];
     }
 }
--- app/Filament/Resources/JobApplicationResource/RelationManagers/InterviewsRelationManager.php
+++ app/Filament/Resources/JobApplicationResource/RelationManagers/InterviewsRelationManager.php
@@ -2,10 +2,7 @@
 
 namespace App\Filament\Resources\JobApplicationResource\RelationManagers;
 
-use App\Enums\InterviewFormatsEnum;
-use App\Enums\InterviewTypesEnum;
 use App\Filament\Resources\InterviewResource;
-use Filament\Forms\Components;
 use Filament\Forms\Form;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
@@ -18,32 +15,7 @@ class InterviewsRelationManager extends RelationManager
     public function form(Form $form): Form
     {
         return $form
-            ->schema([
-                Components\TextInput::make('name')
-                    ->required()
-                    ->maxLength(255),
-                Components\DatePicker::make('interview_date')
-                    ->required(),
-                Components\Select::make('type')
-                    ->options(collect(InterviewTypesEnum::cases())->mapWithKeys(function ($enum) {
-                        return [$enum->value => $enum->name];
-                    }))
-                    ->required(),
-                Components\Select::make('format')
-                    ->options(collect(InterviewFormatsEnum::cases())->mapWithKeys(function ($enum) {
-                        return [$enum->value => $enum->name];
-                    }))
-                    ->required(),
-                Components\Select::make('length_minutes')
-                    ->label('Duration (minutes)')
-                    ->options(
-                        collect(range(5, 180, 5))->mapWithKeys(fn($v) => [
-                            $v => str_pad(floor($v / 60) . 'h ', 4, ' ', STR_PAD_RIGHT) . str_pad($v % 60 . 'm', 3, '0', STR_PAD_LEFT)
-                        ])
-                    )
-                    ->required(),
-                Components\Textarea::make('notes')
-            ]);
+            ->schema(InterviewResource::getSharedFormFields());
     }
 
     public function table(Table $table): Table
